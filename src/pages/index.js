1cba0ce557bd6b41341044abe8a5b7245ca7d60c
Jesse Szepieniec <jessems@gmail.com>
2019-10-13 20:00:05 +0200

    First commits

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,10 +50,7 @@ const IndexPage = ({
     <Wrapper>
       <Hero>
         <h1>Hi.</h1>
-        <p>
-          I&apos;m John Doe, a Senior UX Developer with five years of industry experience, specializing in developing
-          React apps with the best UX users can get.
-        </p>
+        <p>We partner with businesses helping them become more efficient by leveraging technology.</p>
         <Link to="/contact">
           <Button big>
             <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
